c190620a023c332004a761fa9115fa630204818e
Alexander Häusler <alexander.haeusler2@gmail.com>
2015-01-19 11:51:58 +0100

    JUMP_VELOCITY, GRAVITY updated

--- src/de/kbgame/util/Physic.java
+++ src/de/kbgame/util/Physic.java
@@ -13,9 +13,9 @@ import de.kbgame.map.Level;
 
 public final class Physic {
 
-	public static final double JUMP_VELOCITY = 6.0;
+	public static final double JUMP_VELOCITY = 9.0;
 	public static final double WALK_VELOCITY = 1.0;
-	public static final double GRAVITY = 0.11;
+	public static final double GRAVITY = 0.33;
 	public static final double FRICTION = 0.8;
 
 	private Physic() {

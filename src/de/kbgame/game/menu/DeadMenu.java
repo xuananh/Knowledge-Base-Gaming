6bfcdc17bfe93c3257f9ab971f9b1702af7117c6
Nguyen Xuan Anh <ngxanh88@gmail.com>
2015-03-16 01:33:40 +0100

    deadMenu and GoalMenu function

--- src/de/kbgame/game/menu/DeadMenu.java
+++ src/de/kbgame/game/menu/DeadMenu.java
@@ -1,52 +1,54 @@
 package de.kbgame.game.menu;
 
-import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
+import java.awt.event.KeyEvent;
 
 import de.kbgame.game.Game;
+import de.kbgame.util.GameState;
 
 public class DeadMenu extends Menu{
 
-	private static final int X_START = 250;
-	private static final int Y_START = 250;
+	private static final int X_START = 300;
+	private static final int Y_START = 300;
 
 	@Override
 	protected void updateMenu(Game g) {
-		// TODO Auto-generated method stub
+		if (g.input.getKey(KeyEvent.VK_UP) || g.input.getKey(KeyEvent.VK_DOWN)){
+			menuPunkt = (menuPunkt == 1) ? 2 : 1;
+		}
 		
+		if (g.input.getKey(KeyEvent.VK_ENTER)){
+			switch (menuPunkt) {
+			case 1:
+				g.state = GameState.GAME;
+				g.newGame();
+				break;
+			case 2:
+				g.shouldApplicationExit = true;
+				break;
+			default:
+				break;
+			}
+		}
 	}
 
 	@Override
 	public void draw(Game g) {
-		try {
-			BufferedImage image = ImageIO.read(new File("Images/menu_background.png"));
-			g.graphic.currentGrafic.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.4f));
-			g.graphic.currentGrafic.drawImage(image, 0, 0, g.graphic.Width, g.graphic.Height, null);
-			g.graphic.currentGrafic.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		gameLogo(0, 50, g, 0.5f);
-		g.graphic.drawText("Game Over!!", X_START - 30, Y_START - 100, Color.red, false, new Font("Comic Sans MS", Font.BOLD, 50));
 		
-		g.graphic.drawText("Restart Level", X_START, Y_START+50, (menuPunkt == 2) ? Color.green : new Color(255,69,0), false, new Font("Comic Sans MS", Font.BOLD, (menuPunkt == 2) ? 35 : 30));
-		g.graphic.drawText("About & Help", X_START, Y_START + 100, (menuPunkt == 3) ? Color.green : new Color(	255,69,0), false, new Font("Comic Sans MS", Font.BOLD, (menuPunkt == 3) ? 35 : 30));
-		g.graphic.drawText("Exit game", X_START, Y_START + 150, (menuPunkt == 4) ? Color.green : new Color(255,69,0), false, new Font("Comic Sans MS", Font.BOLD, (menuPunkt == 4) ? 35 : 30));
+		gameLogo(10, 50, g, 0.5f);
+		g.graphic.drawText("Game Over!!", X_START - 30, Y_START - 150, Color.red, false, new Font("Comic Sans MS", Font.BOLD, 50));
+		
+		g.graphic.drawText("Score: ", X_START - 20, Y_START - 100, Color.red, false, new Font("Comic Sans MS", Font.BOLD, 30));
+		
+		g.graphic.drawText("Restart Level", X_START, Y_START, (menuPunkt == 1) ? Color.green : new Color(255,69,0), false, new Font("Comic Sans MS", Font.BOLD, (menuPunkt == 1) ? 35 : 30));
+		g.graphic.drawText("Exit game", X_START, Y_START + 50, (menuPunkt == 2) ? Color.green : new Color(255,69,0), false, new Font("Comic Sans MS", Font.BOLD, (menuPunkt == 2) ? 35 : 30));
 
 		int x = X_START - 30;
 		int y = Y_START - 10;
 		switch (menuPunkt) {
 		case 2:
 			y = y + 50;
-			break;
-		case 3:
-			y = y + 100;
 			break;
 		default:
 			break;
--- src/de/kbgame/game/menu/GoalMenu.java
+++ src/de/kbgame/game/menu/GoalMenu.java
@@ -15,29 +15,30 @@ import de.kbgame.util.GameState;
 
 public class GoalMenu extends Menu{
 
-	private static final int X_START = 250;
-	private static final int Y_START = 250;
+	private static final int X_START = 300;
+	private static final int Y_START = 300;
 
 	@Override
 	protected void updateMenu(Game g) {
 		if (g.input.getKey(KeyEvent.VK_UP)){
-			menuPunkt = (menuPunkt == 1) ? 4 : menuPunkt -1;
+			menuPunkt = (menuPunkt == 1) ? 3 : menuPunkt -1;
 		}
 		
 		if (g.input.getKey(KeyEvent.VK_DOWN)){
-			menuPunkt = (menuPunkt == 4) ? 1 : menuPunkt +1;
+			menuPunkt = (menuPunkt == 3) ? 1 : menuPunkt +1;
 		}
 		
 		if (g.input.getKey(KeyEvent.VK_ENTER)){
 			switch (menuPunkt) {
 			case 1:
+				g.state = GameState.GAME;
+				g.newGame();
 				break;
 			case 2:
+				g.state = GameState.GAME;
 				g.newGame();
 				break;
 			case 3:
-				break;
-			case 4:
 				g.shouldApplicationExit = true;
 				break;
 			default:
@@ -56,13 +57,14 @@ public class GoalMenu extends Menu{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		gameLogo(0, 50, g, 0.5f);
-		g.graphic.drawText("Victory!!", X_START - 30, Y_START - 100, Color.magenta, false, new Font("Comic Sans MS", Font.BOLD, 50));
+		gameLogo(10, 50, g, 0.5f);
+		g.graphic.drawText("Victory! Congratulate!", X_START - 150, Y_START - 150, Color.magenta, false, new Font("Comic Sans MS", Font.BOLD, 50));
+		
+		g.graphic.drawText("Score: ", X_START - 20, Y_START - 100, Color.red, false, new Font("Comic Sans MS", Font.BOLD, 30));
 		
 		g.graphic.drawText("Next Level", X_START, Y_START, (menuPunkt == 1) ? Color.green : new Color(255,69,0), false, new Font("Comic Sans MS", Font.BOLD, (menuPunkt == 1) ? 35 : 30));
 		g.graphic.drawText("Restart Level", X_START, Y_START+50, (menuPunkt == 2) ? Color.green : new Color(255,69,0), false, new Font("Comic Sans MS", Font.BOLD, (menuPunkt == 2) ? 35 : 30));
-		g.graphic.drawText("About & Help", X_START, Y_START + 100, (menuPunkt == 3) ? Color.green : new Color(	255,69,0), false, new Font("Comic Sans MS", Font.BOLD, (menuPunkt == 3) ? 35 : 30));
-		g.graphic.drawText("Exit game", X_START, Y_START + 150, (menuPunkt == 4) ? Color.green : new Color(255,69,0), false, new Font("Comic Sans MS", Font.BOLD, (menuPunkt == 4) ? 35 : 30));
+		g.graphic.drawText("Exit game", X_START, Y_START + 100, (menuPunkt == 3) ? Color.green : new Color(255,69,0), false, new Font("Comic Sans MS", Font.BOLD, (menuPunkt == 3) ? 35 : 30));
 
 		int x = X_START - 30;
 		int y = Y_START - 10;
@@ -72,9 +74,6 @@ public class GoalMenu extends Menu{
 			break;
 		case 3:
 			y = y + 100;
-			break;
-		case 4:
-			y = y + 150;
 			break;
 		default:
 			break;

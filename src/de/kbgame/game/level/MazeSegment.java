dc528e28e4543410cb09a9562bcfdae342ea4839
Alexander Häusler <alexander.haeusler2@gmail.com>
2015-03-17 20:31:08 +0100

    maze segment updated => doesn't work with current clingo map loader!

--- src/de/kbgame/game/level/MazeSegment.java
+++ src/de/kbgame/game/level/MazeSegment.java
@@ -1,12 +1,24 @@
 package de.kbgame.game.level;
 
+import java.awt.Point;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 import de.kbgame.game.Game;
+import de.kbgame.map.MapLoader;
 
 
 public class MazeSegment extends LevelSegment {
 	
-	public MazeSegment(Game g, String[] args) {
+	public MazeSegment(Game g, String[] args) throws FileNotFoundException {
 		super(g);
+		
+		goal = new Point();
+		playerStart = new Point();
+		
+		map = MapLoader.loadFromClingo(g, new File(args[0]), playerStart, goal);
+		width = map.length;
+		height = map[0].length;
 	}
 
 }
